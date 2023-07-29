fa2ba843f8382aed6a4d0d84c7c2913cdba1dd8a
Wallace Rossini <rossiniwallace@gmail.com>
2023-07-29 18:32:31 -0300

    feat: Input de dados

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,19 +1,27 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#131016",
+    backgroundColor: '#131016',
     padding: 24,
   },
   eventName: {
-    color:"#FDFCFE",
+    color:'#FFF',
     fontSize: 24,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     marginTop: 48
   },
   eventDate: {
-    color:"#6b6b6b",
-        fontSize: 16
+    color:'#6b6b6b',
+    fontSize: 16
+  },
+  input: {
+    height: 56,
+    backgroundColor: '#1F1E25',
+    borderRadius: 5,
+    color: '#FFF',
+    padding: 16,
+    fontSize: 16
   }
 })
