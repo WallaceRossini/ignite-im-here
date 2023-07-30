2d71f505bc3ab0dd423ba364ce606aa7bff4ec8b
Wallace Rossini <rossiniwallace@gmail.com>
2023-07-29 23:41:12 -0300

    feat: Exibindo Alertas

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,5 @@
 
-import { Text, View, TextInput, TouchableOpacity, FlatList} from 'react-native';
+import { Text, View, TextInput, TouchableOpacity, FlatList, Alert} from 'react-native';
 import { styles } from './styles';
 
 import { Participant } from '../../components/Participant'
@@ -14,11 +14,22 @@ export function Home() {
   ]
 
   function handleParticipantAdd() {
-    console.log('Você clicou no botão de Adicionar!')
+    if(participants.includes('Wallace')){
+      return Alert.alert('Participante existe','Já existe um participante na lista com esse nome!')
+    }
   }
 
   function handleParticipantRemove(name: string) {
-    console.log(`Você clicou em remover o participante ${name}`)
+    Alert.alert('Remover',`Remover o participante ${name} ?`,[
+      {
+        text: 'Sim',
+        onPress: () => Alert.alert('Deletado!')
+      },
+      {
+        text: 'Não',
+        style: 'cancel'
+      }
+    ]);
   }
 
   return (
